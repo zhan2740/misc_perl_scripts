ad003f4e837a0206043412e3bef6c8bbc8ce6da2
Liangliang Gao <lgao@umn.edu>
2015-10-16 10:45:19 -0500

    Update get.scaffold.per.chr.w7984.pl

--- get.scaffold.per.chr.w7984.pl
+++ get.scaffold.per.chr.w7984.pl
@@ -9,6 +9,7 @@ if (@ARGV <2) {die "usage: please specify input chr_file and fasta_file"; };
 my ($chr_file,$fasta_file);
 GetOptions ("chr_file=s"=>\$chr_file, "fasta_file=s"=>\$fasta_file);
 
+########### reading and taking information regarding chromosome position of scaffolds or contigs
 my (%pos_scaff, %chr, %FH)=();
 open (CHR_FILE, "< $chr_file");
 <CHR_FILE>; #remove header
@@ -19,6 +20,7 @@ while (<CHR_FILE>){
 	$chr{$chr}=1;
 }
 
+################## openning file handles , ready to print to each chromosome
 foreach my $key (sort keys %chr){
 	my $file=$key . ".fa";
 	open (my $fh, ">", $file);
@@ -27,6 +29,7 @@ foreach my $key (sort keys %chr){
 
 open (OUT2, ">> not.mapped.fa") or die "cannot open output file for not.mapped.fa";
 
+################# This is the main program to separate single genome wide build into chromosome specific builds.
 $/=">"; ## set the record separator to be ">"
 open (FASTA, "< $fasta_file");
 while (<FASTA>){
