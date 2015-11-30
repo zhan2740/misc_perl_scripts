61a4fa2562926b8b4f0dcabf9a015da1911108ee
Liangliang Gao <LGAO@umn.edu>
2015-11-30 16:32:26 -0600

    add a script to filter file2 based on file1 ID

---
+++ extract.based.on.file1.pl
@@ -0,0 +1,33 @@
+#/usr/bin/perl -w
+
+
+if (@ARGV<2){die "please specify file1 file2"};
+
+my $file1= shift;
+
+open (IN1, "<", $file1);
+
+my %id;
+while (<IN1>){
+	$_=~s/[\r|\n]//g;
+	my @F=split "\t";
+	$id{$F[0]}=1;
+}
+
+my $file2=shift;
+open (IN2, "<", $file2);
+my $header=<IN2>;
+
+while (<IN2>){
+	$_=~s/[\r|\n]//g;
+	my $line=$_;
+	my @F=split (/\t/,$line,2);
+	my $tag=$F[0];
+	if (exists $id{$tag}){
+		print $line . "\n";
+	}
+
+}
+
+
+
